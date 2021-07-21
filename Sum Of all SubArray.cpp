0cdeab5fd38b626229adfde0f39d185be7d58a4d
Ramyashri Hebbar <53457351+Ramyashrihebbar@users.noreply.github.com>
2021-07-21 17:26:22 +0530

    Create Sum Of all SubArray.cpp

---
+++ Sum Of all SubArray.cpp
@@ -0,0 +1,25 @@
+//given an array of size n .output sum of each subarray of the given array
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int cur= 0;
+
+    for(int i=0;i<n;i++){
+        cur =0;
+         for(int j=i;j<n;j++){
+
+                cur+=arr[j];
+                cout<<cur<<endl;
+         }
+    }
+return 0;
+}
