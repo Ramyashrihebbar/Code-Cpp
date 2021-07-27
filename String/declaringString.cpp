d8314c4e6bd82186ae2745ecd6ba3df358d1b85f
Ramyashri Hebbar <53457351+Ramyashrihebbar@users.noreply.github.com>
2021-07-27 17:02:42 +0530

    Update declaringString.cpp

--- String/declaringString.cpp
+++ String/declaringString.cpp
@@ -1,1 +1,20 @@
-//yet to complete
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+
+    /* string str;
+    string str1(5,'n');
+    cout<<str1<<endl; */
+
+   /*  string  str = "Ramya";
+    cout<<str; */
+
+    string str;
+    getline(cin,str);//full sentence
+   // cin>>str;//one word from a given sentence
+    cout<<str<<endl;
+    return 0;
+}
